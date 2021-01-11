7f446890bff7cbc19ff4a71ce86b399a462484c1
pantokrator <pantokrator3841@gmail.com>
2021-01-12 01:10:46 +0900

    Initial commit

---
+++ Assets/Scripts/ColorViewer.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ColorViewer : MonoBehaviour
+{
+    public MultiSourceManager _MultiManager;
+    public RawImage _ColorImage;
+
+    void Update()
+    {
+        _ColorImage.texture = _MultiManager.GetColorTexture();
+    }
+}
